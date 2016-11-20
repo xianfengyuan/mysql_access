14178f40d72f2c1d63d2053bd1720e63ce86e2d8
Xianfeng Yuan <xianfeng.yuan@menlosecurity.com>
2016-11-20 01:15:34 -0800

    add post

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var mysql = require('mysql');
 var app = express();
+var bodyParser = require('body-parser');
 
 var connection = mysql.createConnection({
   host     : 'aurora.cqiybs4tphyt.us-west-2.rds.amazonaws.com',
@@ -15,11 +16,29 @@ app.set('port', 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
   connection.query('SELECT * FROM services', function(err, rows){
     res.render('users', {users : rows});
   });
+});
+app.get('/register', function(req, res){
+   res.sendFile(path.join(__dirname+'/public/register.html'));
+});
+
+app.post('/login',function(req,res){
+   var user_name=req.body.user;
+   var password=req.body.password;
+   var email=req.body.email;
+   var desc=req.body.desc;
+   var price=req.body.price;
+   
+   console.log("User name = "+user_name+", password is "+password);
+   connection.query('INSERT INTO services VALUES (5, ' + user_name + ',' + password + ',' + email + ',' + desc + ',' + price + ')', function(err, rows) {
+      res.send('done');
+   });
 });
 
 app.listen(app.get('port'));
